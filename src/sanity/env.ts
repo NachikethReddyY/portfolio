5811412e92f21925bfbacf54f6eaf6ea49c7749c
Nachiketh Reddy <nachikethreddyy@Nachikeths-MacBook-Air.local>
2026-03-07 21:16:35 +0800

    Updating savity files

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,5 +1,5 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-01-01'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-03-07'
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
@@ -11,11 +11,10 @@ export const projectId = assertValue(
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
-export const useCdn = false
-
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
     throw new Error(errorMessage)
   }
+
   return v
 }
--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,9 +1,5 @@
 import { type SchemaTypeDefinition } from 'sanity'
-import { project } from './project'
-import { techSkill } from './techSkill'
-import { experienceItem } from './experience'
-import { experienceFolder } from './experienceFolder'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-    types: [project, techSkill, experienceItem, experienceFolder],
+  types: [],
 }
